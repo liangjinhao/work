74cc8369ad567d4e6b9ae5957414883288d2c7de
yancheng <bristlegrasses@163.com>
2017-12-22 11:36:09 +0800

    增加监视字典变化动态加载的功能

--- Service/service.py
+++ Service/service.py
@@ -2,7 +2,11 @@ import json
 import time
 import tornado.web
 from tornado.options import options
-
+import os
+import inspect
+import threading
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 import collector
 
 tornado.options.define('port', default=8888, help='run on this port', type=int)
@@ -29,7 +33,9 @@ class MainHandler(tornado.web.RequestHandler):
             # 清洗数据
             text = texts[i]
             if len(text) < 120:  # image_title长度一般不超过120
+                lock.acquire()
                 final_dict = collector_service.collect(text)
+                lock.release()
                 final_result.append(final_dict)
             else:
                 final_result.append('{}')
@@ -47,10 +53,39 @@ class MainHandler(tornado.web.RequestHandler):
         self.get()
 
 
-if __name__ == "__main__":
+class MyThread(threading.Thread):
 
+    def run(self):
+        event_handler = MyHandler()
+        observer = Observer()
+        directory = os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda: 0)))
+        observer.schedule(event_handler, directory, recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except Exception as e:
+            print(e)
+            observer.stop()
+
+
+class MyHandler(FileSystemEventHandler):
+
+    def on_modified(self, event):
+        # /dict/phrase 被修改
+        if event.key[0] == 'modified' and event.key[1].split(r'/')[-1] == 'phrase' and event.key[2] is False:
+            lock.acquire()
+            collector_service.reload_dict()
+            lock.release()
+
+
+if __name__ == "__main__":
     collector_service = collector.Collector()
-    collector_service.collect(' ')
+
+    lock = threading.Lock()
+
+    t = MyThread()
+    t.start()
 
     settings = {
         'template_path': 'views',  # html文件
