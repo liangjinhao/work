74ac7f86c5bf83af808458999caad276695a043f
yancheng <bristlegrasses@163.com>
2018-01-08 11:23:55 +0800

    修改字典更新时间到午夜12点

--- Service/update_dic.py
+++ Service/update_dic.py
@@ -106,5 +106,5 @@ class UpdateDictThread(threading.Thread):
         f = UpdateDict()
         sched = BlockingScheduler()
         # 在每天凌晨 12：00 更新本地字典
-        sched.add_job(f.run, 'cron', hour=12, minute=0)
+        sched.add_job(f.run, 'cron', hour=0, minute=0)
         sched.start()
