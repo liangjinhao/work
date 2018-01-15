444580b2b14a0dcbf0670fb18777b93916828b7f
yancheng <bristlegrasses@163.com>
2018-01-15 16:49:22 +0800

    将useless更改的阈值从1.0改为0.3

--- Service/collector.py
+++ Service/collector.py
@@ -129,7 +129,7 @@ class Collector:
         for i in range(len(crf_result)):
             crf_tag = crf_result[i]['type']
             weight = xgboost_result[i]['weight']
-            if crf_tag == 'useless' and weight > 1.0:
+            if crf_tag == 'useless' and weight > 0.3:
                 new_crf_result.append({'pos': crf_result[i]['pos'], 'term': crf_result[i]['term'], 'type': 'subject4'})
             # elif crf_tag != 'useless' and weight < 0.5:
             #     new_crf_result.append({'pos': crf_result[i]['pos'], 'term': crf_result[i]['term'], 'type': 'useless'})
