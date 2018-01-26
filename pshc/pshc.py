ae4b76a869bf2c585997cb9d05f4bbd2a4d9dae2
yancheng <bristlegrasses@163.com>
2018-01-26 21:32:17 +0800

    从Hbase中的img_data读取出数据，并将数据Post到Solr中

---
+++ pshc/pshc.py
@@ -0,0 +1,279 @@
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+import json
+import datetime
+
+
+INPUTFORMATCLASS = "org.apache.hadoop.hbase.mapreduce.TableInputFormat"
+INPUTKEYCLASS = "org.apache.hadoop.hbase.io.ImmutableBytesWritable"
+INPUTVALUECLASS = "org.apache.hadoop.hbase.client.Result"
+INKEYCONV = "org.apache.spark.examples.pythonconverters.ImmutableBytesWritableToStringConverter"
+INVALUECONV = "org.apache.spark.examples.pythonconverters.HBaseResultToStringConverter"
+
+
+OUTPUTFORMATCLASS = "org.apache.hadoop.hbase.mapreduce.TableOutputFormat"
+OUTPUTKEYCLASS = "org.apache.hadoop.hbase.io.ImmutableBytesWritable"
+OUTPUTVALUECLASS = "org.apache.hadoop.io.Writable"
+OUTKEYCONV = "org.apache.spark.examples.pythonconverters.StringToImmutableBytesWritableConverter"
+OUTVALUECONV = "org.apache.spark.examples.pythonconverters.StringListToPutConverter"
+
+
+CONF = {
+    "hbase.zookeeper.quorum": "emr-header-2.cluster-55102,emr-header-1.cluster-55102,emr-worker-1.cluster-55102"
+}
+
+
+TYPEDICT = {
+    "string": StringType(),
+    "boolean": BooleanType(),
+    "float": FloatType(),
+    "int": IntegerType(),
+    "long": LongType(),
+    "date": DateType(),
+    "datetime": TimestampType()
+}
+
+
+def types_to_string(v, data_type):
+    """
+    transfer other Types to string
+    :param v:
+    :param data_type:
+    :return:
+    """
+    if not v:
+        value = ''
+    elif data_type == 'boolean':
+        value = v and 'True' or 'False'
+    elif data_type == 'date':
+        value = v.strftime("%Y-%m-%d")
+    elif data_type == 'datetime':
+        value = v.strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        value = '%s' % v
+    return value
+
+
+def string_to_types(v, data_type):
+    """
+    transfer string to other Types
+    :param v:
+    :param data_type:
+    :return:
+    """
+    value = None
+    if v:
+        try:
+            if data_type == 'string':
+                value = v
+            elif data_type == 'boolean':
+                value = bool(v)
+            elif data_type == 'float':
+                value = float(v)
+            elif data_type == 'int':
+                value = int(v)
+            elif data_type == 'date':
+                value = datetime.datetime.strptime(v, '%Y-%m-%d')
+            elif data_type == 'datetime':
+                value = datetime.datetime.strptime(v, '%Y-%m-%d %H:%M:%S')
+            else:
+                value = v
+        except:
+            value = None
+    return value
+
+
+class PSHC(object):
+    """
+    PySpark Hbase connector class, used to access Hbase in PySpark
+    """
+
+    def __init__(self, sc, sql_context, conf=CONF):
+        self.conf = conf
+        self.sc = sc
+        self.sqlContext = sql_context
+        self.cache_rdd = {}
+
+    def get_df_from_hbase(self, catelog, cached=True):
+        """
+        Get dataframe from Hbase
+        :param catelog: json , eg:
+        {
+            "table":{"namespace":"default", "name":"table1"},
+            "rowkey":"col0",
+            "columns":{
+              "col0":{"cf":"rowkey", "col":"key", "type":"string"},
+              "col1":{"cf":"cf1", "col":"col1", "type":"boolean"},
+              "col3":{"cf":"cf3", "col":"col3", "type":"float"},
+              "col4":{"cf":"cf4", "col":"col4", "type":"int"},
+              "col5":{"cf":"cf5", "col":"col5", "type":"date"},
+              "col7":{"cf":"cf7", "col":"col7", "type":"datetime"}
+            }
+        }
+        :param cached:
+        :return: :class:`DataFrame`
+        """
+        conf = self.conf.copy()
+        table = catelog['table']['name']
+        conf['hbase.mapreduce.inputtable'] = table
+        hbase_rdd = self.sc.newAPIHadoopRDD(INPUTFORMATCLASS, INPUTKEYCLASS, INPUTVALUECLASS, keyConverter=INKEYCONV,
+                                            valueConverter=INVALUECONV, conf=conf)
+
+        def hrdd_to_rdd(hbase_rdds):
+            new_rdds = []
+            for index, rdd in enumerate(hbase_rdds):
+                values = rdd[1].split("\n")
+                new_value = {}
+                for x in values:
+                    y = json.loads(x)
+                    new_value["%s:%s" % (y['columnFamily'], y['qualifier'])] = y['value']
+                new_rdd = {}
+                for column, v in catelog['columns'].items():
+                    real_v = v['cf'] == 'rowkey' and rdd[0] or new_value.get("%s:%s" % (v['cf'], v['col']), None)
+                    new_rdd[column] = string_to_types(real_v, v['type'])
+                new_rdds.append(new_rdd)
+            return new_rdds
+
+        rdds = hbase_rdd.mapPartitions(hrdd_to_rdd)
+
+        if cached:
+            rdds.cache()
+            self.cache_rdd[table] = rdds
+
+        df = self.sqlContext.createDataFrame(rdds, self.catelog_to_schema(catelog))
+
+        return df
+
+    def uncache_rdd(self, tablename):
+        if tablename in self.cache_rdd:
+            self.cache_rdd[tablename].unpersist()
+        return True
+
+    def save_df_to_hbase(self, dataframe, catelog):
+        """
+        Save dataframe to Hbase
+        :param dataframe: :class:`DataFrame`
+        :param catelog: json, eg:
+        {
+            "table":{"namespace":"default", "name":"table1"},
+            "rowkey":"col0",
+            "columns":{
+              "col0":{"cf":"rowkey", "col":"key", "type":"string"},
+              "col1":{"cf":"cf1", "col":"col1", "type":"boolean"},
+              "col3":{"cf":"cf3", "col":"col3", "type":"float"},
+              "col4":{"cf":"cf4", "col":"col4", "type":"int"},
+              "col5":{"cf":"cf5", "col":"col5", "type":"date"},
+              "col7":{"cf":"cf7", "col":"col7", "type":"datetime"}
+            }
+        }
+        :return:
+        """
+
+        def f(rdds):
+            columns = catelog["columns"]
+            rowkey = catelog["rowkey"]
+            newrdds = []
+            for rdd in rdds:
+                rdd_dict = rdd.asDict()
+                key = str(rdd_dict[rowkey])
+                for k, v in rdd_dict.items():
+                    if k in columns and k != rowkey:
+                        newrdds.append(
+                            (key, [key, columns[k]['cf'], columns[k]['col'], types_to_string(v, columns[k]['type'])]))
+            return newrdds
+
+        conf = self.conf.copy()
+        table = catelog['table']['name']
+        conf.update({
+            "mapreduce.outputformat.class": OUTPUTFORMATCLASS,
+            "mapreduce.job.output.key.class": OUTPUTKEYCLASS,
+            "mapreduce.job.output.value.class": OUTPUTVALUECLASS
+        })
+        conf['hbase.mapred.outputtable'] = table
+        dataframe.rdd.mapPartitions(f).saveAsNewAPIHadoopDataset(conf=conf, keyConverter=OUTKEYCONV,
+                                                                 valueConverter=OUTVALUECONV)
+        return True
+
+    @staticmethod
+    def catelog_to_schema(catelog):
+        columns_dict = catelog.get('columns')
+        columns = columns_dict.keys()
+        structtypelist = [StructField(x, TYPEDICT.get(columns_dict[x]['type'], StringType()), True) for x in columns]
+        schema = StructType(structtypelist)
+        return schema
+
+    @staticmethod
+    def demo():
+        """
+        spark-submit --master yarn --executor-memory 4G --executor-cores 2 --num-executors 4
+        --packages
+        org.apache.hbase:hbase:1.1.12,
+        org.apache.hbase:hbase-server:1.1.12,
+        org.apache.hbase:hbase-client:1.1.12,
+        org.apache.hbase:hbase-common:1.1.12
+        --jars
+        ./spark-examples_2.10-1.6.4-SNAPSHOT.jar
+        --conf
+        spark.pyspark.python=/path to virtualenv python path
+        --py-files
+        pshc.py
+
+        :return:
+        """
+
+        from pyspark import SparkConf, SparkContext
+        from pyspark.sql import SparkSession
+
+        conf = SparkConf().setAppName("test")
+        sc = SparkContext(conf=conf)
+        spark = SparkSession(sc)
+        sql_context = SQLContext(sc)
+        tests = PSHC(sc, sql_context)
+
+        catelog1 = {
+            "table": {"namespace": "default", "name": "test1"},
+            "rowkey": "id",
+            "domain": "",
+            "columns": {
+                "id": {"cf": "rowkey", "col": "key", "type": "string"},
+                "number": {"cf": "info", "col": "number", "type": "int"},
+                "message": {"cf": "info", "col": "message", "type": "string"}
+            }
+        }
+
+        catelog2 = {
+            "table": {"namespace": "default", "name": "test2"},
+            "rowkey": "id",
+            "domain": "",
+            "columns": {
+                "id": {"cf": "rowkey", "col": "key", "type": "string"},
+                "count": {"cf": "info", "col": "count", "type": "int"},
+                "information": {"cf": "info", "col": "information", "type": "string"}
+            }
+        }
+
+        # create dataframe1 and save to Hbase
+        df1 = sql_context.createDataFrame([{'id': '1', 'number': 3, 'message': 'message1'},
+                                           {'id': '2', 'number': 4, 'message': 'message2'},
+                                           {'id': '3', 'number': 5, 'message': 'message3'}])
+        tests.save_df_to_hbase(df1, catelog1)
+
+        # create dataframe2 and save to Hbase
+        df2 = sql_context.createDataFrame([{'id': '1', 'count': 4, 'information': 'information1'},
+                                           {'id': '2', 'count': 5, 'information': 'information2'},
+                                           {'id': '3', 'count': 6, }])
+        tests.save_df_to_hbase(df2, catelog2)
+
+        df_1 = tests.get_df_from_hbase(catelog1)
+        df_1.show()
+        df_2 = tests.get_df_from_hbase(catelog2)
+        df_2.show()
+
+        df_1.registerTempTable('table_test1')
+        df_2.registerTempTable('table_test2')
+
+        new_df = spark.sql("SELECT table_test1.id, table_test1.number, table_test1.message "
+                           "FROM table_test1 JOIN table_test2 ON table_test1.id == table_test2.id")
+        new_df.show()
+        new_df.filter("information IS NOT null AND number > 3")
+        new_df.show()
---
+++ pshc/send_image.py
@@ -0,0 +1,179 @@
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+import time
+import datetime
+import pshc
+import requests
+
+
+"""
+local mode:
+spark-submit --master local --driver-memory 4G --executor-cores 2 --num-executors 4 
+--packages 
+org.apache.hbase:hbase:1.1.12,
+org.apache.hbase:hbase-server:1.1.12,
+org.apache.hbase:hbase-client:1.1.12,
+org.apache.hbase:hbase-common:1.1.12
+--jars 
+/mnt/disk1/data/chyan/work/post_image/spark-examples_2.10-1.6.4-SNAPSHOT.jar 
+--conf spark.pyspark.python=/mnt/disk1/data/chyan/virtualenv/bin/python 
+--py-files /mnt/disk1/data/chyan/work/post_image/pshc.py /mnt/disk1/data/chyan/work/post_image/send_image.py
+
+yarn mode:
+spark-submit --master yarn --executor-memory 4G --executor-cores 2 --num-executors 4 
+--packages 
+org.apache.hbase:hbase:1.1.12,
+org.apache.hbase:hbase-server:1.1.12,
+org.apache.hbase:hbase-client:1.1.12,
+org.apache.hbase:hbase-common:1.1.12
+--jars 
+/mnt/disk1/data/chyan/work/post_image/spark-examples_2.10-1.6.4-SNAPSHOT.jar 
+--conf spark.pyspark.python=/mnt/disk1/data/chyan/virtualenv/bin/python 
+--py-files /mnt/disk1/data/chyan/work/post_image/pshc.py /mnt/disk1/data/chyan/work/post_image/send_image.py
+"""
+
+
+def send(x):
+
+    img_json = {
+        "id": "",  # ggg + 图片url的md5 + _1
+        "image_id": "",  # ggg + 图片url的md5
+        "file_id": "",
+        "image_title": "",  # 图片 title
+        "image_legends": "",
+        "image_url": "",  # 图片 oss_url
+        "file_url": "",
+        "source_url": "",   # 文章 url
+        "chart_type": "",  # 图片类型, 比如: 'LINE_AREA', 'LINE_COLUMN'
+        "bitmap_type": "",
+        "type": "市场研报",  # 固定值, '市场研报'
+        "publish": "",  # 网站 url
+        "company": "",
+        "author": "",
+        "industry": "",
+        "summary": "",
+        "chart_data": "",
+
+        "confidence": 1.0,
+        "time": 0,  # 图的时间
+        "year": 0,  # 图的年份
+        "doc_score": 1.0,  # 默认填写1, 如果图片类型是需要的置 1.0，其余的则置 0.001
+        "stockcode": "",
+        "title": "",  # 文章 title
+        "chart_version": 1,
+        "doc_feature": "",
+        "tags": "",
+        "language": "1"  # 中文为1，英文为2
+        }
+
+    result = []
+
+    post_imgs = []
+    post_count = 0
+
+    for row in x:
+
+        row_key = row['id']
+        img_oss = row['img_oss']
+        img_title = row['img_title'] if 'img_title' in row else ''
+        img_type = row['img_type'] if 'img_type' in row else ''
+        title = row['title'] if 'title' in row else ''
+        url = row['url'] if 'url' in row else ''
+        index_state = row['index_state'] if 'index_state' in row else '0'
+
+        img_json['id'] = 'ggg' + row_key + '_1'
+        img_json['image_id'] = 'ggg' + row_key
+        img_json['image_title'] = img_title
+        img_json['image_url'] = img_oss
+        img_json['source_url'] = url
+
+        if img_type is None:
+            img_type = '[]'
+        img_type = eval(img_type)
+        if img_type:
+            img_types = []
+            [img_types.append(i['type']) for i in img_type]
+            img_types.sort(reverse=True)
+
+            if len(img_types) == 1:
+                img_json['chart_type'] = img_types[0]
+            else:
+                img_json['chart_type'] = '_AND_'.join(img_types)
+        else:
+            img_json['chart_type'] = "OTHER"
+
+        img_json['publish'] = url.lstrip('https?://').split('/')[0]
+
+        img_time = '2018-01-01'
+        img_datetime = datetime.datetime.strptime(img_time, '%Y-%m-%d')
+
+        img_json['time'] = time.mktime(img_datetime.timetuple())
+        img_json['year'] = img_datetime.year
+
+        # 图片类型共有15种：  OTHER, OTHER_MEANINGFUL, AREA_CHART, BAR_CHART, CANDLESTICK_CHART, COLUMN_CHART,
+        # LINE_CHART, PIE_CHART, LINE_CHART_AND_AREA_CHART, LINE_CHART_AND_COLUMN_CHART, GRID_TABLE, LINE_TABLE,
+        # INFO_GRAPH, TEXT, QR_CODE
+        if img_json['chart_type'] == "" or img_json['chart_type'] == "OTHER" or \
+                img_json['chart_type'] == "OTHER_MEANINGFUL":
+            img_json['doc_score'] = 0.01
+        elif img_json['chart_type'] == "QR_CODE" or img_json['chart_type'] == "TEXT":
+            img_json['doc_score'] = 0.1
+        elif img_json['chart_type'] == "INFO_GRAPH":
+            img_json['doc_score'] = 0.5
+        else:
+            img_json['doc_score'] = 1.0
+
+        img_json['title'] = title
+
+        if index_state != 1:
+
+            # 累计1000条post一次数据
+            if post_count < 1000:
+                post_imgs.append(img_json)
+                post_count += 1
+            else:
+                requests.post('http://10.168.20.246:8080/solrweb/chartIndexByUpdate?origin_from=chart', json=post_imgs)
+                post_imgs = []
+                post_count = 0
+
+            new_row = dict()
+            new_row['id'] = row_key
+            new_row['index_state'] = '1'
+
+            result.append(new_row)
+
+    requests.post('http://10.168.20.246:8080/solrweb/chartIndexByUpdate?origin_from=chart', json=post_imgs)
+
+    return result
+
+
+conf = SparkConf().setAppName("Send_Image")
+sc = SparkContext(conf=conf)
+sqlContext = SQLContext(sc)
+connector = pshc.PSHC(sc, sqlContext)
+
+catelog = {
+    "table": {"namespace": "default", "name": "img_data"},
+    "rowkey": "id",
+    "columns": {
+        "id": {"cf": "rowkey", "col": "key", "type": "string"},
+        "img_format": {"cf": "info", "col": "img_format", "type": "string"},
+        "img_oss": {"cf": "info", "col": "img_oss", "type": "string"},
+        "img_title": {"cf": "info", "col": "img_title", "type": "string"},
+        "img_type": {"cf": "info", "col": "img_type", "type": "string"},
+        "img_url": {"cf": "info", "col": "img_url", "type": "string"},
+        "title": {"cf": "info", "col": "title", "type": "string"},
+        "url": {"cf": "info", "col": "url", "type": "string"},
+        "index_state": {"cf": "info", "col": "index_state", "type": "string"},
+    }
+}
+
+df = connector.get_df_from_hbase(catelog)
+df.show(10)
+df.filter("index_state IS null OR index_state != 1")
+df.show(10)
+
+new_rdd = df.rdd.mapPartitions(lambda x: send(x))
+new_df = sqlContext.createDataFrame(new_rdd, connector.catelog_to_schema(catelog))
+new_df.show(10)
+connector.save_df_to_hbase(new_df, catelog)
