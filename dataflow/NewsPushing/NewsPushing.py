0736182d1350a56920e34f3247ea0c638efe71e5
yancheng <bristlegrasses@163.com>
2018-02-10 14:40:42 +0800

    修改bug

--- dataflow/NewsPushing/NewsPushing.py
+++ dataflow/NewsPushing/NewsPushing.py
@@ -2,8 +2,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 import datetime
 import pshc
-import HTML_Tools
-import time
+import Utils
 import requests
 
 """
@@ -37,7 +36,6 @@ org.apache.hbase:hbase-common:1.1.12
 
 def send(x):
 
-    t1 = time.time()
     for row in x:
 
         news_json = dict({
@@ -61,20 +59,20 @@ def send(x):
         news_json['id'] = row['id']
         news_json['author'] = row['author']
 
-        news_json['author'] = HTML_Tools.HTMLTools().author_norm(row['author']) \
+        news_json['author'] = Utils.author_norm(row['author']) \
             if row['author'] is not None else row['author']
 
         news_json['channel'] = row['channel']
         news_json['contain_image'] = row['contain_image']
 
-        news_json['content'] = HTML_Tools.HTMLTools().content_norm(row['content']) \
+        news_json['content'] = Utils.content_norm(row['content']) \
             if row['content'] is not None else row['content']
 
         news_json['crawl_time'] = row['crawl_time']
         news_json['brief'] = row['dese']
         news_json['source_url'] = row['laiyuan']
 
-        normed_publish_time = HTML_Tools.HTMLTools().time_norm(row['publish_time'])
+        normed_publish_time = Utils.time_norm(row['publish_time'])
         news_json['publish_time'] = normed_publish_time if normed_publish_time != '' else row['crawl_time']
 
         news_json['source_name'] = row['source']
@@ -87,41 +85,39 @@ def send(x):
         else:
             news_json['time'] = 0
 
-        t2 = time.time()
-        print('ProcessTime: ', t2-t1)
         requests.post('http://10.168.20.246:8080/solrweb/indexByUpdate?single=true&core_name=core_news', json=[news_json])
-        print('ProcessTime: ', time.time()-t1)
 
 
-conf = SparkConf().setAppName("Push_News")
-sc = SparkContext(conf=conf)
-sqlContext = SQLContext(sc)
-connector = pshc.PSHC(sc, sqlContext)
+if __name__ == '__main__':
+    conf = SparkConf().setAppName("Push_News")
+    sc = SparkContext(conf=conf)
+    sqlContext = SQLContext(sc)
+    connector = pshc.PSHC(sc, sqlContext)
 
-catelog = {
-    "table": {"namespace": "default", "name": "news_data"},
-    "rowkey": "id",
-    "columns": {
-        "id": {"cf": "rowkey", "col": "key", "type": "string"},
-        "author": {"cf": "info", "col": "author", "type": "string"},
-        "channel": {"cf": "info", "col": "channel", "type": "string"},
-        "contain_image": {"cf": "info", "col": "contain_image", "type": "string"},
-        "content": {"cf": "info", "col": "content", "type": "string"},
-        "crawl_time": {"cf": "info", "col": "crawl_time", "type": "string"},
-        "dese": {"cf": "info", "col": "dese", "type": "string"},
-        "laiyuan": {"cf": "info", "col": "laiyuan", "type": "string"},
-        "publish_time": {"cf": "info", "col": "publish_time", "type": "string"},
-        "source": {"cf": "info", "col": "source", "type": "string"},
-        "title": {"cf": "info", "col": "title", "type": "string"},
-        "url": {"cf": "info", "col": "url", "type": "string"},
+    catelog = {
+        "table": {"namespace": "default", "name": "news_data"},
+        "rowkey": "id",
+        "columns": {
+            "id": {"cf": "rowkey", "col": "key", "type": "string"},
+            "author": {"cf": "info", "col": "author", "type": "string"},
+            "channel": {"cf": "info", "col": "channel", "type": "string"},
+            "contain_image": {"cf": "info", "col": "contain_image", "type": "string"},
+            "content": {"cf": "info", "col": "content", "type": "string"},
+            "crawl_time": {"cf": "info", "col": "crawl_time", "type": "string"},
+            "dese": {"cf": "info", "col": "dese", "type": "string"},
+            "laiyuan": {"cf": "info", "col": "laiyuan", "type": "string"},
+            "publish_time": {"cf": "info", "col": "publish_time", "type": "string"},
+            "source": {"cf": "info", "col": "source", "type": "string"},
+            "title": {"cf": "info", "col": "title", "type": "string"},
+            "url": {"cf": "info", "col": "url", "type": "string"},
+        }
     }
-}
 
-startTime = datetime.datetime.strptime('2018-1-31 11:59:59', '%Y-%m-%d %H:%M:%S').strftime('%s') + '000'
-stopTime = datetime.datetime.strptime('2018-05-01 1:0:0', '%Y-%m-%d %H:%M:%S').strftime('%s') + '000'
+    startTime = datetime.datetime.strptime('2018-1-31 11:59:59', '%Y-%m-%d %H:%M:%S').strftime('%s') + '000'
+    stopTime = datetime.datetime.strptime('2018-05-01 1:0:0', '%Y-%m-%d %H:%M:%S').strftime('%s') + '000'
 
-df = connector.get_df_from_hbase(catelog, start_row=None, stop_row=None, start_time=startTime, stop_time=stopTime,
-                                 repartition_num=None, cached=True)
-df.show(10)
-print('======count=======', df.count())
-df.rdd.foreachPartition(lambda x: send(x))
+    df = connector.get_df_from_hbase(catelog, start_row=None, stop_row=None, start_time=startTime, stop_time=stopTime,
+                                     repartition_num=None, cached=True)
+    df.show(10)
+    print('======count=======', df.count())
+    df.rdd.foreachPartition(lambda x: send(x))
