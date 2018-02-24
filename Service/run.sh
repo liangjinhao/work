f402a17409f100749aa5ba56153a30696a5431da
yancheng <bristlegrasses@163.com>
2018-02-24 15:01:07 +0800

    增加启动和停止的shell脚本

---
+++ Service/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+PID_FILE="pid_file"
+
+# run
+if [ "$1" = "stop" ]; then
+    pid=$(cat $PID_FILE)
+    kill -9 $pid
+else
+    if [ -f "/usr/bin/python3" ]; then
+        nohup /usr/bin/python3 service.py --port=1999 --log_file_prefix=tornado_1999.log "$@" > nohub.log 2>&1 &
+    else
+        nohup python3 service.py --port=1999 --log_file_prefix=tornado_1999.log "$@" > nohub.log 2>&1 &
+    fi
+    echo $! > $PID_FILE
+fi
