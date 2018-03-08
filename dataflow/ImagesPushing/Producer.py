e2fc88315ba8b0ad957e6f5c0d7a6045186d9945
yancheng <bristlegrasses@163.com>
2018-03-08 10:23:12 +0800

    修改

--- dataflow/ImagesPushing/Producer.py
+++ dataflow/ImagesPushing/Producer.py
@@ -37,24 +37,23 @@ class ScrawlImagesProducer(threading.Thread):
 
     def run(self):
 
+        credentials = pika.PlainCredentials(self.username, self.password)
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host=self.RabbitMQ_ip, port=self.RabbitMQ_port,
+                                      virtual_host=self.vhost, credentials=credentials))
+        channel = connection.channel()
+        channel.queue_declare(
+            queue=self.queue_name,
+            durable=True,
+            arguments={
+                "x-dead-letter-exchange": "",
+                "x-dead-letter-routing-key": self.queue_name,
+            }
+        )
+
+        r = redis.Redis(host=self.redis_ip, port=self.redis_port)
+
         while True:
-
-            credentials = pika.PlainCredentials(self.username, self.password)
-            connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=self.RabbitMQ_ip, port=self.RabbitMQ_port,
-                                          virtual_host=self.vhost, credentials=credentials))
-            channel = connection.channel()
-            channel.queue_declare(
-                queue=self.queue_name,
-                durable=True,
-                arguments={
-                    "x-dead-letter-exchange": "",
-                    "x-dead-letter-routing-key": self.queue_name,
-                }
-            )
-
-            r = redis.Redis(host=self.redis_ip, port=self.redis_port)
-
             message = r.lpop(name=self.redis_queue_name)
             if not message:
                 logger_producer.info("Redis 队列中无数据，等待5s再取")
@@ -73,4 +72,5 @@ class ScrawlImagesProducer(threading.Thread):
             else:
                 logger_producer.error("从 Redis 推送数据到 RabbitMQ 失败： " + message)
                 r.rpush(self.redis_queue_name, message)
-            connection.close()
+
+        # connection.close()
