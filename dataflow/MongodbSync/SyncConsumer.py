a768731e8112bb6b8544ace260d907870c98674f
yancheng <bristlegrasses@163.com>
2018-03-19 18:05:27 +0800

    修改

--- dataflow/MongodbSync/SyncConsumer.py
+++ dataflow/MongodbSync/SyncConsumer.py
@@ -4,11 +4,11 @@ import OSSPusher
 
 if __name__ == '__main__':
 
-    for i in range(100):
+    for i in range(40):
         MongoDBPusher.MongoDBPusher().start()
         print('MongoDB 推送线程 %s 开启', i)
 
-    for i in range(50):
+    for i in range(20):
         oss_pusher1 = OSSPusher.OSSPusher()
         oss_pusher1.start()
         print('OSS 推送线程 %s 开启', i)
