f2412824b9beb19ebcf3299864ffcb0a47c74ad6
yancheng <bristlegrasses@163.com>
2018-03-27 15:50:46 +0800

    修改

--- dataflow/MongoDB_Hbase_Sync/Task_1.py
+++ dataflow/MongoDB_Hbase_Sync/Task_1.py
@@ -2,6 +2,7 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext, SparkSession
 import pshc
 import oss2
+import logging
 
 """
 该脚本读取 hb_text 表里的 text_file 字段（存有 OSS 链接）, 是然后取oss文本文件，再把oss 文本文件给写到 Hbase 中
@@ -12,7 +13,7 @@ import oss2
 ACCESS_KEY_ID = 'LTAIxqZZWfBv6jBf'
 ACCESS_KEY_SECRET = 'NEQ966QOxCyXgpAyKkU6THUJ3xDxNV'
 BUCKET_HZ = 'abc-crawler'
-ENDPOINT_HZ = 'oss-cn-hangzhou.aliyuncs.com'
+ENDPOINT_HZ = 'oss-cn-hangzhou-internal.aliyuncs.com'
 
 
 def down_load_oss(x):
@@ -24,7 +25,16 @@ def down_load_oss(x):
         if 'text_file' in row and row['text_file'] is not None:
             file_name = row['text_file'].split('aliyuncs.com/')[-1]
             if not bucket_hz.object_exists(file_name):
-                file_stream = bucket_hz.get_object(file_name)
+                try:
+                    file_stream = bucket_hz.get_object(file_name)
+                except oss2.exceptions.NoSuchKey as e:
+                    logging.error(e)
+                    print(e)
+                    continue
+                except Exception as e:
+                    logging.error(e)
+                    print(e)
+                    continue
                 text = ''
                 for line in file_stream:
                     text = text + line
