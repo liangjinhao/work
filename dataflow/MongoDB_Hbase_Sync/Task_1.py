916e5af3dc298514db57de3d8aaa04cd114a8a8a
yancheng <bristlegrasses@163.com>
2018-03-30 18:01:31 +0800

    修改

--- dataflow/MongoDB_Hbase_Sync/Task_1.py
+++ dataflow/MongoDB_Hbase_Sync/Task_1.py
@@ -3,7 +3,11 @@ from pyspark.sql import SQLContext, SparkSession
 import pshc
 import oss2
 import logging
-import sys
+from thrift.transport import TSocket
+from thrift.protocol import TBinaryProtocol
+from hbase import Hbase
+from hbase.ttypes import *
+
 
 """
 该脚本读取 hb_text 表里的 text_file 字段（存有 OSS 链接）, 是然后取oss文本文件，再把oss 文本文件给写到 Hbase 中
@@ -15,6 +19,46 @@ ACCESS_KEY_ID = 'LTAIxqZZWfBv6jBf'
 ACCESS_KEY_SECRET = 'NEQ966QOxCyXgpAyKkU6THUJ3xDxNV'
 BUCKET_HZ = 'abc-crawler'
 ENDPOINT_HZ = 'oss-cn-hangzhou-internal.aliyuncs.com'
+
+# HBase Thrift 信息
+THRIFT_IP = '10.27.71.108'
+THRIFT_PORT = 9099
+
+
+def save_oss_to_hbase(x):
+
+    bucket_hz = oss2.Bucket(oss2.Auth(ACCESS_KEY_ID, ACCESS_KEY_SECRET), ENDPOINT_HZ, BUCKET_HZ)
+
+    transport = TSocket.TSocket(THRIFT_IP, THRIFT_PORT)
+    transport = TTransport.TBufferedTransport(transport)
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    client = Hbase.Client(protocol)
+    transport.open()
+
+    sh = logging.StreamHandler(sys.stdout)
+    sh.setLevel(logging.DEBUG)
+    logger = logging.getLogger('MyLogger')
+    logger.addHandler(sh)
+
+    for row in x:
+        if 'text_file' in row and row['text_file'] is not None:
+            file_name = row['text_file'].split('aliyuncs.com/')[-1]
+            if bucket_hz.object_exists(file_name):
+                try:
+                    file_stream = bucket_hz.get_object(file_name)
+                    text = str(file_stream.read(), 'utf-8')
+                    mutations = []
+                    key = bytes('data:text', encoding="utf8")
+                    var = bytes(text, encoding="utf8")
+                    if len(var) < 10 * 1024 * 1024:
+                        mutations.append(Hbase.Mutation(column=key, value=var))
+                        client.mutateRow(bytes('hb_text_oss_file', "utf-8"), bytes(row['id'], "utf-8"), mutations, {})
+                except Exception as e:
+                    logging.error(e)
+                    print(e)
+                    continue
+
+    transport.close()
 
 
 def down_load_oss(x):
@@ -68,19 +112,23 @@ if __name__ == '__main__':
     df.show(50, False)
     print('======count=====', df.count())
 
-    result_rdd = df.rdd.repartition(300).mapPartitions(lambda x: down_load_oss(x))
-    result_df = spark_session.createDataFrame(result_rdd, ['id', 'text']).show
+    USE_THRIFT = True
 
-    result_df.show(50, False)
-    print('======count=====', result_df.count())
+    if USE_THRIFT:
+        df.rdd.foreachPartition(lambda x: save_oss_to_hbase(x))
+    else:
+        result_rdd = df.rdd.repartition(1000).mapPartitions(lambda x: down_load_oss(x))
+        result_df = spark_session.createDataFrame(result_rdd, ['id', 'text'])
 
-    catelog2 = {
-        "table": {"namespace": "default", "name": "hb_text_oss_file"},
-        "rowkey": "id",
-        "columns": {
-            "id": {"cf": "rowkey", "col": "key", "type": "string"},
-            "text": {"cf": "data", "col": "text", "type": "string"}
+        result_df.show(50, False)
+        print('======count=====', result_df.count())
+
+        catelog2 = {
+            "table": {"namespace": "default", "name": "hb_text_oss_file"},
+            "rowkey": "id",
+            "columns": {
+                "id": {"cf": "rowkey", "col": "key", "type": "string"},
+                "text": {"cf": "data", "col": "text", "type": "string"}
+            }
         }
-    }
-
-    connector.save_df_to_hbase(result_df, catelog2)
+        connector.save_df_to_hbase(result_df, catelog2)
