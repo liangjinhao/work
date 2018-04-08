c9b9039e2538af38669bb6879e713b4b198cfeb8
yancheng <bristlegrasses@163.com>
2018-04-08 15:57:41 +0800

    修改

--- dataflow/MongodbSync/MongoDBListener.py
+++ dataflow/MongodbSync/MongoDBListener.py
@@ -139,15 +139,17 @@ class MongoDBListener(threading.Thread):
 
                                 if 'pngFile' in doc['o'] and doc['o']['pngFile'] is not None:
                                     pngFile_oss = doc['o']['pngFile']
-                                    doc['o']['pngFile'] = doc['o']['pngFile'].replace('oss-cn-hangzhou',
-                                                                                      'oss-cn-hongkong')
+                                    doc['o']['pngFile'] = doc['o']['pngFile']\
+                                        .replace('abc-crawler.oss-cn', 'hk-crawler.oss-cn')\
+                                        .replace('oss-cn-hangzhou', 'oss-cn-hongkong')
                                     r.sadd(OSS_QUEUE, pngFile_oss)
                                     self.logger.info(str(r.scard(OSS_QUEUE)) + '    Push to Redis OSS queue: ' + pngFile_oss)
 
                                 if 'fileUrl' in doc['o']:
                                     fileUrl_oss = doc['o']['fileUrl']
-                                    doc['o']['fileUrl'] = doc['o']['fileUrl'].replace('oss-cn-hangzhou',
-                                                                                      'oss-cn-hongkong')
+                                    doc['o']['fileUrl'] = doc['o']['fileUrl']\
+                                        .replace('abc-crawler.oss-cn', 'hk-crawler.oss-cn')\
+                                        .replace('oss-cn-hangzhou', 'oss-cn-hongkong')
                                     r.sadd(OSS_QUEUE, fileUrl_oss)
                                     self.logger.info(str(r.scard(OSS_QUEUE)) + '    Push to Redis OSS queue: ' + fileUrl_oss)
 
@@ -156,29 +158,33 @@ class MongoDBListener(threading.Thread):
                                 # 的 fileUrl, html_file, text_file, paragraph_file 字段有 OSS 链接
                                 if 'fileUrl' in doc['o'] and doc['o']['fileUrl'] is not None:
                                     fileUrl_oss = doc['o']['fileUrl']
-                                    doc['o']['fileUrl'] = doc['o']['fileUrl'].replace('oss-cn-hangzhou',
-                                                                                      'oss-cn-hongkong')
+                                    doc['o']['fileUrl'] = doc['o']['fileUrl'] \
+                                        .replace('abc-crawler.oss-cn', 'hk-crawler.oss-cn') \
+                                        .replace('oss-cn-hangzhou', 'oss-cn-hongkong')
                                     r.sadd(OSS_QUEUE, fileUrl_oss)
                                     self.logger.info(str(r.scard(OSS_QUEUE)) + '    Push to Redis OSS queue: ' + fileUrl_oss)
 
                                 if 'html_file' in doc['o'] and doc['o']['html_file'] is not None:
                                     html_file_oss = doc['o']['html_file']
-                                    doc['o']['html_file'] = doc['o']['html_file'].replace('oss-cn-hangzhou',
-                                                                                          'oss-cn-hongkong')
+                                    doc['o']['html_file'] = doc['o']['html_file'] \
+                                        .replace('abc-crawler.oss-cn', 'hk-crawler.oss-cn') \
+                                        .replace('oss-cn-hangzhou', 'oss-cn-hongkong')
                                     r.sadd(OSS_QUEUE, html_file_oss)
                                     self.logger.info(str(r.scard(OSS_QUEUE)) + '    Push to Redis OSS queue: ' + html_file_oss)
 
                                 if 'text_file' in doc['o'] and doc['o']['text_file'] is not None:
                                     text_file_oss = doc['o']['text_file']
-                                    doc['o']['text_file'] = doc['o']['text_file'].replace('oss-cn-hangzhou',
-                                                                                          'oss-cn-hongkong')
+                                    doc['o']['text_file'] = doc['o']['text_file']\
+                                        .replace('abc-crawler.oss-cn', 'hk-crawler.oss-cn')\
+                                        .replace('oss-cn-hangzhou', 'oss-cn-hongkong')
                                     r.sadd(OSS_QUEUE, text_file_oss)
                                     self.logger.info(str(r.scard(OSS_QUEUE)) + '    Push to Redis OSS queue: ' + text_file_oss)
 
                                 if 'paragraph_file' in doc['o'] and doc['o']['paragraph_file'] is not None:
                                     paragraph_file_oss = doc['o']['paragraph_file']
-                                    doc['o']['paragraph_file'] = doc['o']['paragraph_file'].replace('oss-cn-hangzhou',
-                                                                                                    'oss-cn-hongkong')
+                                    doc['o']['paragraph_file'] = doc['o']['paragraph_file']\
+                                        .replace('abc-crawler.oss-cn', 'hk-crawler.oss-cn')\
+                                        .replace('oss-cn-hangzhou', 'oss-cn-hongkong')
                                     r.sadd(OSS_QUEUE, paragraph_file_oss)
                                     self.logger.info(str(r.scard(OSS_QUEUE)) + '    Push to Redis OSS queue: ' + paragraph_file_oss)
 
